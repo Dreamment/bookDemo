8f3d9efd782753adbe5fa8cfaf10a2ff893ae97a
Emre Savaş <emre_savas28@hotmail.com>
2023-10-22 17:23:24 +0300

    Added GET request handling for the bookDemo API.

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,10 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using bookDemo.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace bookDemo.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/books")]
     [ApiController]
     public class BooksController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContext.Books;
+            return Ok(books);
+
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBooks([FromRoute(Name = "id")] int id)
+        {
+            var book = ApplicationContext.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+            if (book is null)
+                return NotFound();
+
+            return Ok(book);
+
+        }
     }
 }
